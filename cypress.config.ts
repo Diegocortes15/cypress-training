f6b1577c3210b5d2ac57cb6a3b81a41d9af08853
Diego <cortesroadiegoalejandro@gmail.com>
2022-07-21 21:07:05 -0500

    setting typescript

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      // modify config values examples
+      // config.defaultCommandTimeout = 10000
+
+      // IMPORTANT return the updated config object
+      return config;
+    }
+  }
+});
